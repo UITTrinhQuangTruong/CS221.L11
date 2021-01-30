386db43cc2c86d0257506ab788a2da44e0f29b85
UITTrinhQuangTruong <18520393@gm.uit.edu.vn>
2021-01-30 14:49:48 +0700

    Fix error file CKY.py

--- CKY.py
+++ CKY.py
@@ -141,7 +141,7 @@ def printTable(lenSentence, TABLE):
                           list(TABLE[i][j][k].values())[0]['X'])
 
 
-def syntacticAnalyzer(sentence, lexicon, grammar, TypeTokenizer, ParserModel):
+def syntacticAnalyzer(sentence, lexicon, grammar):
     '''
         Xay dung cay cu phap tu 1 sentence
 
@@ -189,51 +189,3 @@ def syntacticAnalyzer(sentence, lexicon, grammar, TypeTokenizer, ParserModel):
     return list_Results
 
 
-#  def Parser(intk, Input, Output, lexicon, grammar, TypeTokenizer, ParserModel,
-#          TypeOuput):
-#      s = 0
-#      if intk:
-#          n = int(input('Nhap n'))
-#          list_Sentences = []
-#          for i in range(n):
-#              list_Sentences.append(input())
-#      else:
-#          if Input:
-#              list_Sentences = readSentence(Input)
-#          else:
-#              list_Sentences = readSentence()
-#
-#      list_Results = []
-#      for idx, sentence in enumerate(list_Sentences):
-#          result = syntacticAnalyzer(sentence, lexicon, grammar, TypeTokenizer,
-#                                     ParserModel)
-#
-#          try:
-#              if TypeOuput:
-#                  list_Results.append(result)
-#              else:
-#                  list_Results.append([result[0]])
-#          except:
-#              list_Results.append([])
-#          if len(result) > 0:
-#              s += 1
-#      if Output:
-#          with open(output, 'w') as f:
-#              for i in range(len(list_Sentences)):
-#                  f.write(list_Sentences[i])
-#                  f.write('\n')
-#                  try:
-#                      f.writelines(list_Results[i])
-#                  except:
-#                      pass
-#
-#                  f.write('----------------------------------------------\n')
-#      else:
-#          for i in range(len(list_Sentences)):
-#              print('Num: %d| %s' % (i + 1, list_Sentences[i]))
-#              try:
-#                  for j in list_Results[i]:
-#                      print(j)
-#              except:
-#                  print()
-#              print('----------------------------------------------\n')
